98a532b71909717bdf51ba348cfa67f7e6a392cc
Nero671 <stas.kachalko@mail.ru>
2020-11-03 23:51:33 +0300

    chore: add secondaryFunction

---
+++ reactapp/src/Components/Modal/CountItem.js
@@ -0,0 +1,34 @@
+import React, { useLayoutEffect } from 'react';
+import styled from 'styled-components';
+
+const CountWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  padding: 0 50px 0 37px;
+`;
+
+const CountInput = styled.input`
+  width: 50px;
+  font-size: 20px;
+`;
+
+const ButtonCount = styled.button`
+  background-color: transparent;
+`;
+
+const CountItem = ({ count, setCount, onChange }) => {
+
+  return (
+    <CountWrapper>
+      <span>Количество</span>
+      <div>
+        <ButtonCount disabled={count <= 1} onClick={() => setCount(count - 1)}>-</ButtonCount>
+        <CountInput type='number' min='1' max='100' value={count < 1 ? 1 : count} onChange={onChange}/>
+        <ButtonCount onClick={() => setCount(count + 1)}>+</ButtonCount>
+      </div>
+    </CountWrapper>
+  )
+}
+
+export default CountItem;
